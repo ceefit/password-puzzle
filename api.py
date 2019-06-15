b626953e7895c2b14751432dc66775bea9cf89e6
ceefit <ceefit@protonmail.com>
2019-06-15 00:48:42 -0400

    Dockerize

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
+import json
+
 from aiohttp import web_request, web
 
 
 async def health(request: web_request):
-    return web.Response(text="OK")
+    response = json.dumps({'status': 'ok'})
+    return web.Response(body=response)
 
---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+REGEXP = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+
+
+def read_version():
+
+    init_py = os.path.join(os.path.dirname(__file__), '__init__.py')
+
+    with open(init_py) as f:
+        for line in f:
+            match = REGEXP.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            msg = f'Cannot find version in ${init_py}'
+            raise RuntimeError(msg)
+
+
+install_requires = [
+    'aiohttp==3.5.4',
+    'gunicorn==19.9.0',
+]
+
+
+setup(
+    name='password-puzzle',
+    version=read_version(),
+    description='',
+    platforms=['POSIX'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    zip_safe=False,
+)
